8ddb8b17e43476adcc39afda920196d4ef587a70
Jeff Walsh <zorderelda@gmail.com>
2021-12-20 13:30:23 -0500

    Move loader to uml, moved run out of uml to loader

--- loader.py
+++ loader.py
@@ -1,34 +1,4 @@
-import os
-from flask import Flask
-from datetime import timedelta
-from dotenv import load_dotenv
+from uml import create_app
 
-def create_app():
-
-    # Add the configuration from the dotenv
-    APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
-    dotenv_path = os.path.join(APP_ROOT, '.flaskenv')
-    load_dotenv(dotenv_path)
-
-    app = Flask(__name__)
-
-    # Setup session stuff
-    app.permanent_session_lifetime = timedelta(minutes=5)
-
-    # register blueprint
-    from main import main
-    app.register_blueprint(main.bp)
-
-    # Register stuff
-    from main.main import sses, jwt
-    sses.init_app(app)
-    jwt.init_app(app)
-
-    @app.context_processor
-    def utility_jwt_encode():
-        return dict(get_token=jwt.encode_jwt_token)
-
-    return app
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     create_app().run()

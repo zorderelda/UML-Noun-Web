fe8867387525820d1d670cedbc90e38a154dd01b
Jeff Walsh <zorderelda@gmail.com>
2021-12-30 14:16:44 -0500

    Fixed errors

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -69,15 +69,8 @@ window.onload = function(event)
 
 async function performFetch(apiurl, options, func)
 {
-<<<<<<< HEAD
-    // Stop interaction
-    turnOffInteraction()
-
-    // Get the JWT
-=======
     //let apiurl = document.querySelector('meta[name=data-api]').getAttribute('content') + 'fill';
     let apiurl = window.location.protocol + '//' + window.location.hostname + '/fill';
->>>>>>> 4e1361118d30e5ed74a664dcf6bf4bb2c957f15b
     let jwt = document.querySelector('meta[name=token]').getAttribute('content');
 
     // Add the 
@@ -138,15 +131,8 @@ async function doUpload()
 
 async function doDownload(value)
 {
-<<<<<<< HEAD
-    let apiurl = new URL(document.querySelector('meta[name=data-api]').getAttribute('content') + 'download');
-    //let apiurl = new URL(window.location.protocol + '//' + window.location.hostname + '/download');
-
-    // Get the JWT
-=======
     //let apiurl = new URL(document.querySelector('meta[name=data-api]').getAttribute('content') + 'download');
     let apiurl = new URL(window.location.protocol + '//' + window.location.hostname + '/download');
->>>>>>> 4e1361118d30e5ed74a664dcf6bf4bb2c957f15b
     let jwt = document.querySelector('meta[name=token]').getAttribute('content');
 
     // Send the request to the api
@@ -156,54 +142,3 @@ async function doDownload(value)
     // Get the file
     window.location = apiurl.href
 }
-<<<<<<< HEAD
-
-function turnOffInteraction()
-{
-    // Disable download button
-    let button = document.getElementById('top-dl-button');
-    button.setAttribute('disabled', '');
-    button.children[0].classList.add('visually-hidden');
-    button.children[1].classList.remove('visually-hidden');
-    button.children[2].classList.remove('visually-hidden');
-}
-
-function turnOnInteraction()
-{
-    // Enable download button
-    let button = document.getElementById('top-dl-button');
-    button.removeAttribute('disabled', '');
-    button.children[0].classList.remove('visually-hidden');
-    button.children[1].classList.add('visually-hidden');
-    button.children[2].classList.add('visually-hidden');
-}
-
-function disableInteraction()
-{
-    // Disable download button
-    let button = document.getElementById('top-dl-button')
-    button.setAttribute('disabled', '');
-    button.children[0].classList.remove('visually-hidden');
-    button.children[1].classList.add('visually-hidden');
-    button.children[2].classList.add('visually-hidden');
-}
-
-function fillText(data)
-{
-    if(data)
-    {
-        // Put text into the areas
-        document.getElementById('paperview').innerHTML = data['paperview'];
-        document.getElementById('tableview').innerHTML = data['tableview'];
-    }
-
-    else
-    {
-        // Put text into the areas
-        document.getElementById('paperview').innerHTML = '';
-        document.getElementById('tableview').innerHTML = '';
-    }
-    
-}
-=======
->>>>>>> 4e1361118d30e5ed74a664dcf6bf4bb2c957f15b
